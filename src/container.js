dcaaa134dc5ffd2fe8d7e97e716cb521c8e41c93
Sohaib <sohaibsaleem89@gmail.com>
2024-03-28 15:32:28 +0500

    Add two more card

--- src/container.js
+++ src/container.js
@@ -31,6 +31,34 @@ export default function Container() {
           </div>
         </div>
       </div>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h2>Card 1</h2>
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, voluptatem.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h2>Card 2</h2>
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, voluptatem.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

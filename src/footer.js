7e9b226a3f466a26b5a404c4ba21bc3f83004150
shaharyar <shaharyar@gmail.con>
2024-03-28 15:04:25 +0500

    header 2 added

--- src/footer.js
+++ src/footer.js
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="bg-dark text-primary">footer</div>;
+  return <div className="bg-dark text-primary">footer
+  
+  <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0 " >
+        <div className="container ">
+          <a className="navbar-brand" href="#">IQbal <span className='text-warning'>Trading</span> Company</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent ">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 " >
+              <li className="nav-item me-2 home_menu">
+                <a className="nav-link active text-white " aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item me-2 menu_hover">
+                <a className="nav-link text-white" href="#">Services</a>
+              </li>
+
+              <li className="nav-item me-2 menu_hover" >
+                <a className="nav-link text-white" href="#" tabindex="-1" >About Us</a>
+              </li>
+
+              <li className="nav-item me-2 menu_hover">
+                <a className="nav-link text-white" href="#" tabindex="-1" >Contect Us</a>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>;
 }
